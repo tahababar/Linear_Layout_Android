5b5732622f375663e22cd6af812fa39a8a009f8a
Taha Babar <tahababar70@gmail.com>
2022-05-15 14:14:13 -0400

    Add files via upload

---
+++ src/main/java/com/tahababar/homework2tahababar/TipActivity.java
@@ -0,0 +1,14 @@
+package com.tahababar.homework2tahababar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip);
+    }
+}
